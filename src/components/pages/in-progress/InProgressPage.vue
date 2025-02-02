572e02b90a5d907f765dd5dc1242f58a292ef064
KimYuriy <komrad.kim-j@yandex.ru>
2025-02-02 16:42:45 +0300

    feat(InProgressPage): Добавлена страница 'В разработке', также добавлен путь в роутер

---
+++ src/components/pages/in-progress/InProgressPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container
+      class="fill-height"
+      fluid
+    >
+      <v-row
+        class="text-center text-h2 text-uppercase"
+        align="center"
+        justify="center"
+      >
+        <v-col cols="12">
+          В разработке
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
+  <style scoped>
+  .fill-height {
+    height: 100vh;
+  }
+  </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import HomePage from '@/components/pages/home/HomePage.vue'
+import InProgressPage from '@/components/pages/in-progress/InProgressPage.vue';
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
     path: '/',
     component: HomePage,
-  }
+  },
+  {
+    path: '/in-progress',
+    component: InProgressPage,
+  },
+
 ];
 
 const router = createRouter({
