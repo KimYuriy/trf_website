3ddfe99ecfc47099f238c4674dfb4478129f788e
KimYuriy <komrad.kim-j@yandex.ru>
2025-03-16 21:28:42 +0300

    feat(social media alignment): Карточки соцсетей отображаются в 2 столбика на экранах 960+ пикселей, если таких карточек 6+

--- src/components/pages/home/ui/socialmedia/SocialMedia.vue
+++ src/components/pages/home/ui/socialmedia/SocialMedia.vue
@@ -4,18 +4,33 @@
     fluid
   >
     <custom-title text="Социальные сети" />
+    <div :class="mdAndUp? `${socials.length > 5 ? 'grid-container' : ''}` : ''">
       <social-media-card
         v-for="media in socials"
+        :key="media.id"
         :media="media"
       />
+    </div>
   </v-container>
 </template>
+
 <script setup lang="ts">
+import { useDisplay } from 'vuetify';
 import type { DataModel } from '@/components/pages/home/model/all_data';
 import CustomTitle from '@/components/pages/home/ui/common/CustomTitle.vue';
 import SocialMediaCard from '@/components/pages/home/ui/socialmedia/ui/socialmedia-card/SocialMediaCard.vue';
+
+const { mdAndUp } = useDisplay();
 
 defineProps<{
   socials: DataModel[]
 }>();
 </script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 16px;
+}
+</style>
