3bbfb713c9c70219d7a4cff9d22a02fdb0a7a8ce
KimYuriy <komrad.kim-j@yandex.ru>
2025-03-16 12:15:06 +0300

    feat(glitch effect): Добавлен глитч-эффект для логотипов

--- src/components/pages/home/ui/about-org-n-events/ui/logos/InteractiveLogos.vue
+++ src/components/pages/home/ui/about-org-n-events/ui/logos/InteractiveLogos.vue
@@ -4,77 +4,41 @@
       align="center"
       justify="center"
     >
-        <v-col cols="3">
-          <v-tooltip
-            location="end"
-            :text="dvudulka.shortDescription"
-          >
-            <template #activator="{ props }">
-              <v-img
-                v-bind="props"
-                cover
-                aspect-ratio="1"
-                :src="dvudulka.logo"
-                @click="emit('eventClicked', dvudulka.id)"
-              />
-            </template>
-          </v-tooltip>
-        </v-col>
+          <v-col cols="3">
+            <glitch-logo
+              tooltip-location="end"
+              :event="dvudulka"
+              @event-clicked="eventClicked"
+            />
+          </v-col>
         <v-row
           class="ma-0 pa-0"
           align="end"
           justify="center"
         >
           <v-col cols="3">
-            <v-tooltip
-              location="top"
-              :text="edc.shortDescription"
-            >
-              <template #activator="{ props }">
-                <v-img
-                  v-bind="props"
-                  cover
-                  aspect-ratio="1"
-                  :src="edc.logo"
-                  @click="emit('eventClicked', edc.id)"
-                />
-              </template>
-            </v-tooltip>
+            <glitch-logo
+              tooltip-location="top"
+              :event="edc"
+              @event-clicked="eventClicked"
+            />
           </v-col>
           <v-col
             class="pb-16"
             cols="5"
           >
-            <v-tooltip
-              location="bottom"
-              :text="zh.shortDescription"
-            >
-              <template #activator="{ props }">
-                <v-img
-                  v-bind="props"
-                  cover
-                  aspect-ratio="1"
-                  :src="zh.logo"
-                  @click="emit('eventClicked', zh.id)"
-                />
-              </template>
-            </v-tooltip>
+            <glitch-logo
+              tooltip-location="bottom"
+              :event="zh"
+              @event-clicked="eventClicked"
+            />
           </v-col>
           <v-col cols="3">
-            <v-tooltip
-              location="top"
-              :text="rus.shortDescription"
-            >
-              <template #activator="{ props }">
-                <v-img
-                  v-bind="props"
-                  cover
-                  aspect-ratio="1"
-                  :src="rus.logo"
-                  @click="emit('eventClicked', rus.id)"
-                />
-              </template>
-            </v-tooltip>
+            <glitch-logo
+              tooltip-location="top"
+              :event="rus"
+              @event-clicked="eventClicked"
+            />
           </v-col>
         </v-row>
     </v-col>
@@ -83,6 +47,7 @@
 
 <script setup lang="ts">
 import type { EventModel } from '@/components/pages/home/model/all_data';
+import GlitchLogo from '@/components/pages/home/ui/about-org-n-events/ui/logos/GlitchLogo.vue';
 
 interface ILogos {
   zh: EventModel;
@@ -96,4 +61,6 @@ defineProps<ILogos>();
 const emit = defineEmits<{
   eventClicked: [id: number]
 }>();
+
+const eventClicked = (id: number) => emit('eventClicked', id);
 </script>
